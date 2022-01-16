893bb23f746f696209464c9981db938bcfecfa3f
DanInvents <55188328+DanInvents@users.noreply.github.com>
2022-01-16 10:19:10 +0200

    Delete Firmware/Version 1.0 directory

--- Firmware/Version 1.0/batteryStatus.ino
+++
@@ -1,8 +0,0 @@
-void batteryStatus(){
-  if ((2*analogRead(29)*3.3/(pow(2,12)-1)) < 3.8){
-    digitalWrite(batLED, HIGH);
-  }
-  else if ((2*analogRead(29)*3.3/(pow(2,12)-1)) > 3.8){
-    digitalWrite(batLED, LOW);
-  }
-}
